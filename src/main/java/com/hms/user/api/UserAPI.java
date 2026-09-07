3dcf83c45bfa246c941a0bd963d85542e3f413dc
SureshGandham18 <20pa1a0540@vishnu.edu.in>
2026-09-07 17:43:52 +0530

    Initial commit

---
+++ src/main/java/com/hms/user/api/UserAPI.java
@@ -0,0 +1,38 @@
+package com.hms.user.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hms.user.dto.ResponseDTO;
+import com.hms.user.dto.UserDTO;
+import com.hms.user.exception.HmsException;
+import com.hms.user.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+@CrossOrigin
+public class UserAPI {
+    @Autowired
+    private UserService userService;
+    @PostMapping("/register")
+    public ResponseEntity<ResponseDTO> registerUser(@RequestBody @Valid UserDTO userDTO) throws HmsException {
+        userService.registerUser(userDTO);
+        return new ResponseEntity<>(new ResponseDTO("User registered successfully"), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> loginUser(@RequestBody @Valid UserDTO userDTO) throws HmsException {
+        UserDTO loggedInUser = userService.loginUser(userDTO);
+        return new ResponseEntity<>(loggedInUser, HttpStatus.OK);
+    }
+}
