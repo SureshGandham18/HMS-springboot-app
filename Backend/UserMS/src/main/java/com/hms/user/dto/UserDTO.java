97884967016118e7fa4a10cd230250dcbabadd8a
SureshGandham18 <20pa1a0540@vishnu.edu.in>
2026-09-16 17:57:07 +0530

    Add HMS frontend and microservices

---
+++ Backend/UserMS/src/main/java/com/hms/user/dto/UserDTO.java
@@ -0,0 +1,30 @@
+package com.hms.user.dto;
+
+import com.hms.user.entity.User;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
+    @NotBlank(message = "Password is mandatory")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
+    private String password;
+    private Roles role;
+
+    public User toEntity() {
+        return new User(this.id, this.name, this.email, this.password, this.role);
+    }
+}
